93a949ca9dd5b084e50957de63c75ed0d2e42ad9
Timur <Stafr95@mail.ru>
2024-05-07 19:53:36 +0500

    Practice working with Middleware

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,15 @@
-import { legacy_createStore as createStore, combineReducers, compose } from "redux";
+import { legacy_createStore as createStore, combineReducers, compose, applyMiddleware } from "redux";
 import heroes from "../reducers/heroes";
 import filters from "../reducers/filters";
+
+const stringMiddleware = () => (next) => (action) => {
+	if (typeof action === 'string') {
+		return next({
+			type: action
+		})
+	}
+	return next(action)
+}
 
 const enhancer = (createStore) => (...args) => {
 	const store = createStore(...args);
@@ -19,9 +28,13 @@ const enhancer = (createStore) => (...args) => {
 
 const store = createStore(
 																		combineReducers({heroes, filters}),
-																		compose(
-																			enhancer,
-																			window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-																		))
+																		compose(applyMiddleware(stringMiddleware),
+																			window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+
+																		// compose(
+																		// 	enhancer,
+																		// 	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+																		// )
+																		);
 
 export default store;
