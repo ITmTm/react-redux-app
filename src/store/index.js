a581b09f268188916911645a4aafbf8858ac0eb5
Timur <Stafr95@mail.ru>
2024-05-07 20:27:56 +0500

    Import library "thunk"

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,5 @@
 import { legacy_createStore as createStore, combineReducers, compose, applyMiddleware } from "redux";
+import { thunk as ReduxThunk } from "redux-thunk";
 import heroes from "../reducers/heroes";
 import filters from "../reducers/filters";
 
@@ -28,7 +29,7 @@ const enhancer = (createStore) => (...args) => {
 
 const store = createStore(
 																		combineReducers({heroes, filters}),
-																		compose(applyMiddleware(stringMiddleware),
+																		compose(applyMiddleware(ReduxThunk, stringMiddleware),
 																			window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 																		// compose(
