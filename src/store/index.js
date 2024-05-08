437aa4e27d8eaa4d2e7113bd62e57777d10a1ec6
Timur <Stafr95@mail.ru>
2024-05-08 16:06:21 +0500

    Usage Redux Toolkit "configureStore"

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,4 @@
-import { legacy_createStore as createStore, combineReducers, compose, applyMiddleware } from "redux";
-import { thunk as ReduxThunk } from "redux-thunk";
+import { configureStore } from "@reduxjs/toolkit";
 import heroes from "../reducers/heroes";
 import filters from "../reducers/filters";
 
@@ -12,30 +11,58 @@ const stringMiddleware = () => (next) => (action) => {
 	return next(action)
 }
 
-const enhancer = (createStore) => (...args) => {
-	const store = createStore(...args);
 
-	const oldDispatch = store.dispatch;
-	store.dispatch = (action) => {
-		if (typeof action === 'string') {
-			return oldDispatch({
-				type: action
-			})
-		}
-		return oldDispatch(action)
-	}
-	return store;
-}
+	// Redux Toolkit
+const store = configureStore({
+	reducer: {heroes, filters},
+	middleware: getDefaultMiddleware => getDefaultMiddleware().concat(stringMiddleware),
+	devTools: process.env.NODE_ENV !== 'production'
+})
 
-const store = createStore(
-																		combineReducers({heroes, filters}),
-																		compose(applyMiddleware(ReduxThunk, stringMiddleware),
-																			window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
-																		// compose(
-																		// 	enhancer,
-																		// 	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-																		// )
-																		);
+
+
+  // Redux
+
+// import { legacy_createStore as createStore, combineReducers, compose, applyMiddleware } from "redux";
+// import { thunk as ReduxThunk } from "redux-thunk";
+
+// const store = createStore(
+// 	combineReducers({heroes, filters}),
+// 	compose(applyMiddleware(ReduxThunk, stringMiddleware),
+// 		window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+// );
+
+
+
+
+
+
+
+// const enhancer = (createStore) => (...args) => {
+// 	const store = createStore(...args);
+//
+// 	const oldDispatch = store.dispatch;
+// 	store.dispatch = (action) => {
+// 		if (typeof action === 'string') {
+// 			return oldDispatch({
+// 				type: action
+// 			})
+// 		}
+// 		return oldDispatch(action)
+// 	}
+// 	return store;
+// }
+
+// const store = createStore(
+// 																		combineReducers({heroes, filters}),
+// 																		compose(applyMiddleware(ReduxThunk, stringMiddleware),
+// 																			window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+//
+// 																		// compose(
+// 																		// 	enhancer,
+// 																		// 	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+// 																		// )
+// 																		);
 
 export default store;
