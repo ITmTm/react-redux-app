34a8efb3db5223311317991ddf55002ff054f96a
Timur <Stafr95@mail.ru>
2024-05-10 19:55:38 +0500

    Usage React.StrictMode

--- src/index.js
+++ src/index.js
@@ -9,10 +9,10 @@ import './styles/index.scss'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+	<React.StrictMode>
 		<Provider store={store}>
 			<App />
 		</Provider>
-  </React.StrictMode>
+	</React.StrictMode>
 );
 
