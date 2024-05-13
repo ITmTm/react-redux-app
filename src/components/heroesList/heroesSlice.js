4b57b9c4a2c2f1293d6b7f6f98bacfb236be6f80
Timur <Stafr95@mail.ru>
2024-05-13 17:48:21 +0500

    Using "createEntityAdapter" and wrapping the filteredHeroesSelector function

--- src/components/heroesList/heroesSlice.js
+++ src/components/heroesList/heroesSlice.js
@@ -1,10 +1,16 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk, createEntityAdapter, createSelector } from "@reduxjs/toolkit";
 import {useHttp} from "../../hooks/http.hook";
 
-const initialState = {
-		heroes: [],
-		heroesLoadingStatus: 'idle'
-}
+const heroesAdapter = createEntityAdapter()
+
+// const initialState = {
+// 		heroes: [],
+// 		heroesLoadingStatus: 'idle'
+// }
+
+const initialState = heroesAdapter.getInitialState({
+	heroesLoadingStatus: 'idle'
+});
 
 export const fetchHeroes = createAsyncThunk(
 	'heroes/fetchHeroes',
@@ -27,10 +33,12 @@ const heroesSlice = createSlice({
 		// },
 		// heroesFetchingError: state => {state.heroesLoadingStatus = 'error'},
 		heroCreated: (state, action) => {
-			state.heroes.push(action.payload);
+			// state.heroes.push(action.payload); -- Вместо этого теперь .addOne
+			heroesAdapter.addOne(state, action.payload);
 		},
 		heroDeleted: (state, action) => {
-			state.heroes = state.heroes.filter(item => item.id !== action.payload);
+			// state.heroes = state.heroes.filter(item => item.id !== action.payload); -- Аналогично, .removeOne
+			heroesAdapter.removeOne(state, action.payload);
 		}
 	},
 	extraReducers: (builder) => {
@@ -38,7 +46,8 @@ const heroesSlice = createSlice({
 			.addCase(fetchHeroes.pending, state => {state.heroesLoadingStatus = 'loading'})
 			.addCase(fetchHeroes.fulfilled, (state, action) => {
 				state.heroesLoadingStatus = 'idle';
-				state.heroes = action.payload;
+				heroesAdapter.setAll(state, action.payload);
+				// state.heroes = action.payload;  Удален, вместо его heroesAdapter
 			})
 			.addCase(fetchHeroes.rejected, state => {state.heroesLoadingStatus = 'error'})
 			.addDefaultCase(() => {})
@@ -48,4 +57,19 @@ const heroesSlice = createSlice({
 const {actions, reducer} = heroesSlice;
 
 export default reducer;
+
+const {selectAll} = heroesAdapter.getSelectors(state => state.heroes);
+
+export const filteredHeroesSelector = createSelector(
+	(state) => state.filters.activeFilter,
+	selectAll,
+	(filter, heroes) => {
+		if (filter === 'all') {
+			return heroes;
+		} else {
+			return heroes.filter(item => item.element === filter);
+		}
+	}
+);
+
 export const {heroCreated, heroDeleted} = actions;
