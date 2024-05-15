72156c77027e339a22060d1188177ebd49108f76
Timur <Stafr95@mail.ru>
2024-05-15 19:41:38 +0500

    Importing a custom hook

--- src/components/heroesList/HeroesList.js
+++ src/components/heroesList/HeroesList.js
@@ -4,7 +4,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { CSSTransition, TransitionGroup } from "react-transition-group";
 
 import { heroDeleted, fetchHeroes } from "./heroesSlice";
-import { useGetHeroesQuery } from "../../api/apiSlice";
+import { useGetHeroesQuery, useDeleteHeroMutation } from "../../api/apiSlice";
 
 import HeroesListItem from "../heroesListItem/HeroesListItem";
 import Spinner from '../spinner/Spinner';
