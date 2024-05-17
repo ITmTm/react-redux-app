3356a7a52a0d68eab9cda18da8f184907c204992
Timur <Stafr95@mail.ru>
2024-05-17 23:37:00 +0500

    project build

--- src/api/apiSlice.js
+++ src/api/apiSlice.js
@@ -1,37 +1,8 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const jsonServer = require('json-server')
-
-const server = jsonServer.create()
-
-// Uncomment to allow write operations
-// const fs = require('fs')
-// const path = require('path')
-// const filePath = path.join('db.json')
-// const data = fs.readFileSync(filePath, "utf-8");
-// const db = JSON.parse(data);
-// const router = jsonServer.router(db)
-
-// Comment out to allow write operations
-const router = jsonServer.router('heroes.json')
-
-const middlewares = jsonServer.defaults()
-
-server.use(middlewares)
-// Add this before server.use(router)
-server.use(jsonServer.rewriter({
-	'/api/*': '/$1',
-	'/blog/:resource/:id/show': '/:resource/:id'
-}))
-server.use(router)
-server.listen(3000, () => {
-	console.log('JSON Server is running')
-})
-
-
 export const apiSlice = createApi({
 	reducerPath: 'api',
-	baseQuery: fetchBaseQuery({baseUrl: 'http://localhost:3001'}),
+	baseQuery: fetchBaseQuery({baseUrl: '/api'}),
 	tagTypes: ['Heroes'],
 	endpoints: builder => ({
 		getHeroes: builder.query({
